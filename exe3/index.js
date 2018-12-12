28626d1395333794e288a73c37e2145f2fcebc52
Sou59 <sz5962@gmail.com>
2018-12-12 22:09:28 +0100

    correction

--- exe3/index.js
+++ exe3/index.js
@@ -3,7 +3,7 @@ const http = require("http"); // import module qui implante le protocole http
 
 http.createServer((request, response) => {
 
-    // SEND yhe HTTP header
+    // SEND the HTTP header
     // HTTP Status: 200 : OK
     //Content Type: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
